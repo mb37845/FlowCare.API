49fe1b274f54d261964cf25d6ea8502d5857434f
user <user@DESKTOP-O52L4RH>
2026-03-05 03:03:15 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+
+using FlowCare.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
+
+namespace FlowCare.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddDbContext<FlowcareDbContext>(options =>
+            {
+                options.UseNpgsql(builder.Configuration.GetConnectionString("Default"),
+                    providerOptions => providerOptions.EnableRetryOnFailure()); 
+            });
+           
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddOpenApi();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+                app.MapScalarApiReference(options =>
+                {
+                    options.WithTitle("My API");
+                    options.WithTheme(ScalarTheme.BluePlanet);
+                });
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
